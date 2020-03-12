5a171850ffaab79d5fc972637eb1c66d747e318b
vinod77-sudo <61173415+vinod77-sudo@users.noreply.github.com>
2020-03-12 13:06:39 +0530

    Add files via upload

---
+++ testclass_1.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+class Animal:
+    """A simple function of dog"""
+    def __init__(self,name,age):
+        """About the intro"""
+        self.name=name
+        self.age=age
+        print("success fully completed ")
+    def sit(self):
+        """about sitting position of dog"""
+        print(f"{self.name} is in sitting position")
+    def roll_over(self):
+        """about the rollover position"""
+        print(f"{self.name} is rolled over")
+
+
+# In[ ]:
+
+
+
+
